b7b946cd87fabd287b332719de7f9cb0cf043563
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-06-16 18:45:05 +1200

    Create nested_introduction.svelte

---
+++ nested_introduction.svelte
@@ -0,0 +1,22 @@
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+	}
+	
+	h1 {
+		font-size: 2em;
+		color: goldenrod
+	}
+</style>
+
+<h1>This is an h1</h1>
+<p>This is a paragraph.</p>
+
+
+
+<script>
+	import Nested from './Nested.svelte';
+</script>
+<p>This paragraph comes from Nested.svelte</p>
+<Nested />
