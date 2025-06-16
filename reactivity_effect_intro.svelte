428b8bb19ed57aeeaf9dff8f192a70ee9e1358cd
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-06-17 09:47:47 +1200

    Create reactivity_effect_intro.svelte

---
+++ reactivity_effect_intro.svelte
@@ -0,0 +1,15 @@
+<script>
+	let elapsed = $state(0);
+	let interval = $state(1000);
+
+	$effect(() => {
+		setInterval(() => {
+			elapsed +=1;
+		}, interval);
+	});
+</script>
+
+<button onclick={() => interval /= 2}>speed up</button>
+<button onclick={() => interval *= 2}>slow down</button>
+
+<p>elapsed: {elapsed}</p>
